e70cb958d9c448ff1f747807ce339deffb52896f
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-03-07 18:12:29 -0600

    Added index.js to html skeleton. Added sudo code for indes.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// ask user for name
+// ask for user input (since its only in cli I will make shortcuts)
+// make sure they enter the allowed value
+// while game counter is under 5 games or one user hasn't won 3 times keep asking for input
+// create score values for each player
+// if user wins add a point 
+// if user wins 3 times game over
+// display a grind of scores maybe recent plays
